5b648eccea2c4e44659b184ec73281c00966d726
kimtaehyuk1 <98taehyukkim@naver.com>
2023-03-28 15:12:38 +0900

    feat: question.html 업데이트

--- advance/step2_app_factory/service/controllers/auth_controller.py
+++ advance/step2_app_factory/service/controllers/auth_controller.py
@@ -5,7 +5,7 @@ MVC 패턴 중에 C임.
         - 비즈니스 로직: 사용자가 요청하는 주 내용을 처리하는 곳
 '''
 from flask import render_template, request, url_for
-from service.controllers import bp_auth as auth
+from service.controllers import bp_auth as auth # 블루프린트 땡겨오기
 
 # ~/auth/
 @auth.route('/') # 등록한 bluepritn도 라우팅할수 있다.
--- advance/step2_app_factory/service/controllers/main_controller.py
+++ advance/step2_app_factory/service/controllers/main_controller.py
@@ -4,7 +4,7 @@ MVC 패턴 중에 C임.
         - 라우트 : URL과 이를 처리할 함수 연계
         - 비즈니스 로직: 사용자가 요청하는 주 내용을 처리하는 곳
 '''
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from service.controllers import bp_main as main # controllers 패키지의 init이니까, blueprint객체 들고오기
 from service.forms import FormQuestion
 
@@ -13,7 +13,9 @@ from service.forms import FormQuestion
 def home():
     return render_template('index.html')
 
-@main.route('/question') # 등록한 bluepritn도 라우팅할수 있다.
+@main.route('/question', methods=['GET','POST']) # 등록한 bluepritn도 라우팅할수 있다.
 def question():
-    form = FormQuestion()
+    form = FormQuestion()  #폼 유효성 검사통과하고
+    if request.method == 'POST' and form.validate_on_submit():
+        return redirect( url_for('main_bp.home') ) # ~/main
     return render_template('question.html', wtf=form)
